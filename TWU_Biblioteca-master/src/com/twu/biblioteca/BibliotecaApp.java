b4bd64a1cd99e67087d48982983b0416a9eedc7c
Wen Fu <wen.fu@thoughtworks.com>
2020-08-01 22:04:16 +0800

    [Wen Fu] {1.4} add a option to menu.

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,20 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         BibliotecaApp app = new BibliotecaApp();
         System.out.println(app.WelcomeMessage());
-        Book book = new Book();
-        System.out.println(book.toList());
+        System.out.println("Menu: 1.List of books");
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
+        switch(option)
+        {
+            case 1:{Book book = new Book();
+            System.out.println(book.toList());}
+        }
     }
 
     public String WelcomeMessage() {
