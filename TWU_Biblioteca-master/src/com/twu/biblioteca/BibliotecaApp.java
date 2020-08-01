c14d1bb8a401fa63e78cda1d49cda015f5985885
Wen Fu <wen.fu@thoughtworks.com>
2020-08-01 21:12:39 +0800

    [Wen Fu] {1.1} show welcome message.

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+        System.out.println(app.WelcomeMessage());
+    }
+
+    public String WelcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
 }
--- TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.assertEquals;
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldReturnWelcomeMessage() {
+        BibliotecaApp app = new BibliotecaApp();
+        String result = app.WelcomeMessage();
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", result);
     }
 }
