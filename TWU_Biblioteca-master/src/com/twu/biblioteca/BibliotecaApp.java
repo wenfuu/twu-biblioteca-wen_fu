05e50f702950c0845dc9cb4cfa584bd73069a8b6
Wen Fu <wen.fu@thoughtworks.com>
2020-08-01 22:59:39 +0800

    [Wen Fu] {1.6} add exit option.

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -7,22 +7,27 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         BibliotecaApp app = new BibliotecaApp();
         System.out.println(app.WelcomeMessage());
-        System.out.println("Menu: 1.List of books");
+        System.out.println("Menu: 1.List of books 2.Exit");
         Scanner sc = new Scanner(System.in);
         int option = sc.nextInt();
-        switch(option)
-        {
-            case 1:{Book book = new Book();
-                System.out.println(book.toList());
-                break;}
+        app.menu(option);
+    }
+
+    public String WelcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public void menu(int option) {
+        switch(option) {
+            case 1: {Book book = new Book();
+                System.out.println(book.toList());break;}
+            case 2 : {
+                System.exit(0);
+            }
             default :{
                 System.out.println("Please select a valid option!");
                 break;
             }
         }
-    }
-
-    public String WelcomeMessage() {
-        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
 }
